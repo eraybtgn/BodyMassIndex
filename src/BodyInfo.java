cfaaa3f447ef0ebc0662de4fd8f88a3024ef3fdd
Eray <86014603+eraybtgn@users.noreply.github.com>
2022-11-04 17:22:17 +0300

    Update BodyInfo.java

--- src/BodyInfo.java
+++ src/BodyInfo.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class BodyInfo {
+import java.util.Scanner;
+
+public class BodyInfo {
+    public double height;
+    public double weight;
+    public double endeks;
+
+    public BodyInfo() {
+    }
+
+    public BodyInfo(double height, int weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+    public double vucutEndeks(){
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Lütfen metre cinsinden boyunuzu giriniz.");
+        height= scan.nextDouble();
+        System.out.println("Lütfen kilonuzu giriniz..");
+        weight= scan.nextDouble();
+        endeks= weight/(Math.pow(height,2));
+        System.out.println("Vücut kitle endeksiniz: "+endeks);
+        return endeks;
+    }
 }
